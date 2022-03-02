cdfaf2069cd8c486fe904408b2b1033742f4e800
michael <jingkunwang@hotmail.com>
2022-03-02 10:54:28 +0800

    初期化

---
+++ src/main/java/com/jiz/jiz_authorize/App.java
@@ -0,0 +1,8 @@
+package com.jiz.jiz_authorize;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "jiz-commons" );
+    }
+}
---
+++ src/test/java/com/jiz/jiz_authorize/AppTest.java
@@ -0,0 +1,14 @@
+package com.jiz.jiz_authorize;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
